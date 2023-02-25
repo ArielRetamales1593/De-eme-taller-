2dc46479ac2042a79630875a2f3c2deaed8a9f05
Ariel Retamales <nretamales15@gmail.com>
2023-02-25 12:02:27 -0300

    actualizacion repo

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ItemListContainer = (props) => {
-  let { greeting = " " } = props;
+  let { greeting } = props;
   return (
     <div>
       <h2> {greeting} </h2>
